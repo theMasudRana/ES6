a04f4871169b078c267ec3f79562917dd1752ecc
theMasudRana <mr.masudrana00@gmail.com>
2019-05-06 11:34:02 +0600

    Ep 3 end

--- jsdom.js
+++ jsdom.js
@@ -46,7 +46,7 @@ const before = document.querySelector('.container h1');
 cotainer.insertBefore(newDiv, before); */
 
 
-const removeButton = document.getElementById('remove-button');
+/* const removeButton = document.getElementById('remove-button');
 
 removeButton.addEventListener('click', removeFunc);
 
@@ -58,4 +58,35 @@ function removeFunc(e) {
   console.log(e.clientX);
   const output = document.getElementById('output');
   output.innerHTML = '<h2>' + e.target.id + '</h2>';
+} */
+
+
+/* const button = document.getElementById('remove-button');
+button.addEventListener('click', eventType);
+button.addEventListener('dblclick', eventType);
+button.addEventListener('mousedown', eventType);
+button.addEventListener('mouseup', eventType);
+button.addEventListener('mouseover', eventType);
+button.addEventListener('mouseenter', eventType);
+button.addEventListener('mouseleave', eventType);
+button.addEventListener('mousemove', eventType);
+
+
+function eventType(e) {
+  console.log('Event Type ' + e.type);
+  
+} */
+
+/* const textInput = document.querySelector('input[type="text"]');
+const form = document.querySelector('form'); */
+
+/* textInput.addEventListener('keydown', runEven);
+textInput.addEventListener('focus', runEven);
+textInput.addEventListener('blur', runEven); */
+const tt = document.querySelector('.custom-select');
+tt.addEventListener('change', runEven);
+
+function runEven(e) {
+  console.log(e.type);
+  console.log(e.target.value);
 }
