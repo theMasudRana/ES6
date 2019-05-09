464a222175d969cbe882eae0daf571659db949a8
theMasudRana <mr.masudrana00@gmail.com>
2019-05-09 19:25:10 +0600

    Dom last turorial

--- main.js
+++ main.js
@@ -1,13 +1,19 @@
-const person = {
-    firstName: 'Masud',
-    lastName: 'Rana',
-    age: 24,
-    hobbies: ['Code', 'Nothing', 'Whatever'],
-    address: {
-        street: '124 main st',
-        city: 'Dhaka'
-    }
-};
+const form = document.getElementById('addform');
+const itemList = document.getElementById('items');
 
-console.log(person.firstName, person.address.city);
-console.log(person.hobbies[1]);
+// Form submit event
+form.addEventListener('submit', addItem);
+
+function addItem(e) {
+    e.preventDefault();
+
+    // Get input value
+    const newItem = document.getElementById('item').value;
+    // Create new li element
+    const li = document.createElement('li');
+    li.className = 'list-group-item';
+
+    // Add text node with input value
+    li.appendChild(document.createTextNode(newItem));
+    itemList.appendChild('li');
+}
